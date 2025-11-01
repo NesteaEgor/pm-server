ff6bb905b3d2b8bd2eb864091fd0637626a98d9a
NesteaEgor <ENesterovvvz2004@yandex.ru>
2025-11-01 10:42:30 +0200

    Initial commit

---
+++ src/main/java/ru/nesterov/pmserver/PmServerApplication.java
@@ -0,0 +1,13 @@
+package ru.nesterov.pmserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nesterov/pmserver/PmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nesterov.pmserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
