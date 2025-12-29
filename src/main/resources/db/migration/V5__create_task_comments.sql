cab97b51ddef236e028c36bbdb8ceace9e400cb4
NesteaEgor <ENesterovvvz2004@yandex.ru>
2025-12-29 06:42:36 +0300

    final

--- src/main/resources/db/migration/V5__create_task_comments.sql
+++ src/main/resources/db/migration/V5__create_task_comments.sql
@@ -3,7 +3,7 @@ create table task_comments (
                                task_id uuid not null references tasks(id) on delete cascade,
                                author_id uuid not null references users(id) on delete cascade,
                                text varchar(5000) not null,
-                               created_at timestampz not null default now()
+                               created_at timestamptz not null default now()
 );
 
 create index idx_task_comments_task_id_created_at on task_comments(task_id, created_at);
